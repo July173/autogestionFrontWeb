096cbd55077ed94c862a8215b4f43bf7a9400e5d
Reyving <medinafabriany@gmail.com>
2025-12-08 23:13:25 -0500

    refactor: update PDF URL construction to use environment variable for API base URL

--- src/components/assing/AssignTableView.tsx
+++ src/components/assing/AssignTableView.tsx
@@ -424,7 +424,7 @@ const AssignTableView: React.FC<AssignTableViewProps> = ({
                                 <PdfView
                                   uri={
                                     detail.pdf_url.startsWith("/")
-                                      ? "http://127.0.0.1:8000" + detail.pdf_url
+                                      ? `${import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "")}${detail.pdf_url}`
                                       : detail.pdf_url
                                   }
                                   initialFullscreen={false}
